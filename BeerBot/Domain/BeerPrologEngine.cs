6d7c3672e7a3cd2b24b971f94150e52e7548f592
Jean <jean.b4sset@gmail.com>
2017-03-05 20:11:48 +0100

    first commit

---
+++ BeerBot/Domain/BeerPrologEngine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prolog;
+
+namespace Domain
+{
+    class BeerPrologEngine
+    {
+        public PrologEngine e { get; private set; }
+    }
+}
