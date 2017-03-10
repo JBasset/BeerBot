8ba071d090077a2a4dc86b5a2b95be76ec4fe9e4
Jean <jean.b4sset@gmail.com>
2017-03-10 15:01:20 +0100

    all facts from the database are now prolog facts

--- BeerBot/Domain/OpenBeerDB.cs
+++ BeerBot/Domain/OpenBeerDB.cs
@@ -52,17 +52,22 @@ namespace Domain
             List<string[]> beers = AskDataBase("SELECT id FROM beers", new string[] { "id" });
             foreach (string[] beer in beers)
             {
-                string prologName = "beer" + beer[0];
+                string prologName = "beer" + beer[0]; // Names in the database include a lot of not allowed characters, hence the automatically generated name
                 lines.Add("beer(" + prologName + ").");
             }
             #endregion
 
             #region getting all categories
-            List<string[]> categories = AskDataBase("SELECT cat_name FROM categories", new string[] { "cat_name" });
+            List<string[]> categories = AskDataBase("SELECT id FROM categories", new string[] { "id" });
             foreach(string[] category in categories)
             {
-                string prologName = category[0].Replace(' ', '_').ToLower(); ;
-                lines.Add("category(" + prologName + ").");
+                if (int.Parse(category[0]) != -1)
+                {
+                    string prologName = "category" + category[0]; // Names in the database include a lot of not allowed characters, hence the automatically generated name
+                    lines.Add("category(" + prologName + ").");
+                }
+                else
+                    lines.Add("category(unknownCategory)."); // We need an unknown category, but the name "category-1" isn't acceptable
             }
             #endregion
 
@@ -70,8 +75,85 @@ namespace Domain
             List<string[]> styles = AskDataBase("SELECT id FROM styles", new string[] { "id" });
             foreach (string[] style in styles)
             {
-                string prologName = "style" + style[0];
-                lines.Add("style(" + prologName + ").");
+                if (int.Parse(style[0]) != -1)
+                {
+                    string prologName = "style" + style[0];// Names in the database include a lot of not allowed characters, hence the automatically generated name
+                    lines.Add("style(" + prologName + ").");
+                }
+                else
+                    lines.Add("style(unknownStyle)."); // We need an unknown style, but the name "style-1" isn't acceptable
+            }
+            #endregion
+
+            #region getting all abv (alcohol by volume)
+            List<string[]> abvs = AskDataBase("SELECT id,abv FROM beers", new string[] { "id", "abv" });
+            foreach (string[] abv in abvs)
+            {
+                lines.Add("abv(beer"+ abv[0] + ","+abv[1].Replace(',','.')+")."); // decimals should be separated with a dot, not a comma
+            }
+            #endregion
+
+            #region getting all ibu (internationnal bitterness unit)
+            List<string[]> ibus = AskDataBase("SELECT id,ibu FROM beers", new string[] { "id", "ibu" });
+            foreach (string[] ibu in ibus)
+            {
+                lines.Add("ibu(beer" + ibu[0] + "," + ibu[1].Replace(',', '.') + ")."); // decimals should be separated with a dot, not a comma
+            }
+            #endregion
+
+            #region getting all srm (standard reference method)
+            List<string[]> srms = AskDataBase("SELECT id,srm FROM beers", new string[] { "id", "srm" });
+            foreach (string[] srm in srms)
+            {
+                lines.Add("srm(beer" + srm[0] + "," + srm[1].Replace(',', '.') + ")."); // decimals should be separated with a dot, not a comma
+            }
+            #endregion
+
+            #region getting all beers categories
+            List<string[]> beersCat = AskDataBase("SELECT id, cat_id FROM beers", new string[] { "id", "cat_id" });
+            foreach (string[] beerCat in beersCat)
+            {
+                string beerPrologName = "beer" + beerCat[0]; // Names in the database include a lot of not allowed characters, hence the automatically generated name
+                string catPrologName;
+                if (int.Parse(beerCat[1]) != -1)
+                    catPrologName = "category" + beerCat[1];
+                else
+                    catPrologName = "unknownCategory";
+                lines.Add("beerCategory(" + beerPrologName + "," + catPrologName + ").");
+            }
+            #endregion
+
+            #region getting all beers styles
+            List<string[]> beersSty = AskDataBase("SELECT id, style_id FROM beers", new string[] { "id", "style_id" });
+            foreach (string[] beerSty in beersSty)
+            {
+                string beerPrologName = "beer" + beerSty[0]; // Names in the database include a lot of not allowed characters, hence the automatically generated name
+                string styPrologName;
+                if (int.Parse(beerSty[1]) != -1)
+                    styPrologName = "style" + beerSty[1];
+                else
+                    styPrologName = "unknownStyle";
+                lines.Add("beerStyle(" + beerPrologName + "," + styPrologName + ").");
+            }
+            #endregion
+
+            #region getting all styles categories
+            List<string[]> stylesCat = AskDataBase("SELECT id, cat_id FROM styles", new string[] { "id", "cat_id" });
+            foreach (string[] styleCat in stylesCat)
+            {
+                string stylePrologName;
+                if (int.Parse(styleCat[0]) != -1)
+                    stylePrologName = "style" + styleCat[0];
+                else
+                    stylePrologName = "unknownStyle";
+
+                string catPrologName;
+                if (int.Parse(styleCat[1]) != -1)
+                    catPrologName = "category" + styleCat[1];
+                else
+                    catPrologName = "unknownCategory";
+
+                lines.Add("styleCategory(" + stylePrologName + "," + catPrologName + ").");
             }
             #endregion
 
