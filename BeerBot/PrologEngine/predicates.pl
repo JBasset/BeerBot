37c8c3ab584e1f8b5b76127f32914843c4b16a4f
Jean <jean.b4sset@gmail.com>
2017-03-14 14:37:45 +0100

    adding the sql scripts to create the actual database. The database contains the openbeerdb original tables plus tables users, ratings and social_categories. For now there is only one social category (student), but more will be added later on

--- BeerBot/PrologEngine/predicates.pl
+++ BeerBot/PrologEngine/predicates.pl
@@ -1,1 +1,3 @@
-:- consult("facts.pl").
+% :- consult("facts.pl").
+% this line reads the facts in facts.pl, and is used to test the predicates. However, the C#Prolog engine doesn't recognize the consult command, so it must stay commented while not testing.
+
